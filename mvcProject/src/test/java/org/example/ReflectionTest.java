38028dacc6f44f79193e447b5a1eae4ee3847146
Changho0514 <chh9450@naver.com>
2023-03-30 13:56:42 +0900

    MAKE MVC FRAMEWORK -> refactor: methods

--- mvcProject/src/test/java/org/example/ReflectionTest.java
+++ mvcProject/src/test/java/org/example/ReflectionTest.java
@@ -2,12 +2,15 @@ package org.example;
 
 import javassist.tools.reflect.Reflection;
 import org.example.annotation.Controller;
+import org.example.annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ReflectionTest {
@@ -16,11 +19,16 @@ public class ReflectionTest {
 
     @Test
     void controllerScan() {
-        Reflections reflection = new Reflections("org.example");
-
-        Set<Class<?>> beans = new HashSet<>();
-        beans.addAll(reflection.getTypesAnnotatedWith(Controller.class));
+        Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
 
         logger.debug("beans: [{}]", beans);
+    }
+
+    private Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
+        Reflections reflections = new Reflections("org.example");
+
+        Set<Class<?>> beans = new HashSet<>();
+        annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
+        return beans;
     }
 }
