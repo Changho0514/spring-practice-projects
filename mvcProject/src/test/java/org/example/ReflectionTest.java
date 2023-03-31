9f38142b4a6906f6094c9c6c453a5b789563cd26
Changho0514 <chh9450@naver.com>
2023-03-31 19:38:46 +0900

    MAKE MVC FRAMEWORK -> feat: 3 methods of reflection

--- mvcProject/src/test/java/org/example/ReflectionTest.java
+++ mvcProject/src/test/java/org/example/ReflectionTest.java
@@ -3,15 +3,18 @@ package org.example;
 import javassist.tools.reflect.Reflection;
 import org.example.annotation.Controller;
 import org.example.annotation.Service;
+import org.example.model.User;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ReflectionTest {
 
@@ -24,6 +27,34 @@ public class ReflectionTest {
         logger.debug("beans: [{}]", beans);
     }
 
+    @Test
+    void showClass() {
+        Class<User> clazz = User.class;
+        logger.debug(clazz.getName());
+
+        logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+        logger.debug("User all declared constructors: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
+        logger.debug("User all declared methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+
+    }
+
+    @Test
+    void load() throws ClassNotFoundException {
+        //힙영역에 로드 되어있는 class 타입객체를 가져오는 방법중 1
+        Class<User> clazz = User.class;
+
+        //힙영역에 로드 되어있는 class 타입객체를 가져오는 방법중 2
+        User user = new User("choi", "최창호");
+        Class<? extends User> clazz2 = user.getClass();
+
+        //힙영역에 로드 되어있는 class 타입객체를 가져오는 방법중 3
+        Class<?> clazz3 = Class.forName("org.example.model.User");
+
+        logger.debug("clazz: [{}]", clazz);
+        logger.debug("clazz2: [{}]", clazz2);
+        logger.debug("clazz3: [{}]", clazz3);
+    }
+
     private Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
         Reflections reflections = new Reflections("org.example");
 
@@ -31,4 +62,6 @@ public class ReflectionTest {
         annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
         return beans;
     }
+
+
 }
