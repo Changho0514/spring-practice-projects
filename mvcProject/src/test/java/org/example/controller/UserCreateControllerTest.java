f27a0ea6bfea91627ec2009a1113be4b018e3ba6
Changho0514 <chh9450@naver.com>
2023-04-08 17:45:00 +0900

    MAKE MVC FRAMEWORK -> feat:UserCreateController

--- mvcProject/src/test/java/org/example/controller/UserCreateControllerTest.java
+++ mvcProject/src/test/java/org/example/controller/UserCreateControllerTest.java
@@ -3,5 +3,13 @@ package org.example.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserCreateControllerTest {
-
+    @Test
+    public void () throws Exception {
+        //given
+        
+        //when
+        
+        //then
+        
+    }
 }
