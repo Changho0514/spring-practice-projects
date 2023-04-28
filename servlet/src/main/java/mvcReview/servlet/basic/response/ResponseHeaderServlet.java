41640d5c1f014fa2ca24dc8e92b363c9aeb4970a
Changho0514 <chh9450@naver.com>
2023-04-29 01:00:36 +0900

    Review Inflearn class : Refactor : Cookie

--- servlet/src/main/java/mvcReview/servlet/basic/response/ResponseHeaderServlet.java
+++ servlet/src/main/java/mvcReview/servlet/basic/response/ResponseHeaderServlet.java
@@ -6,10 +6,12 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@Slf4j
 @WebServlet(name = "ResponseHeaderServlet", urlPatterns = "/response-header")
 public class ResponseHeaderServlet extends HttpServlet {
     @Override
@@ -21,7 +23,7 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.setHeader("Cache-control", "no-cache, no-store, must-revalidate");
         response.setHeader("Pragma", "no-cache");
         response.setHeader("my-header", "hello");
-
+        response.setHeader("Set-Cookie", "myCookie");
         //[Header 편의 메서드]
         content(response);
         cookie(response);
@@ -41,7 +43,10 @@ public class ResponseHeaderServlet extends HttpServlet {
     private void cookie(HttpServletResponse response) {
         Cookie cookie = new Cookie("myCookie", "good");
         cookie.setMaxAge(600); // 600초
+        log.info("setMaxAge 이후");
         response.addCookie(cookie);
+        log.info("addCookie 이후");
+//        response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
     }
 
     private void redirect(HttpServletResponse response) throws IOException {
