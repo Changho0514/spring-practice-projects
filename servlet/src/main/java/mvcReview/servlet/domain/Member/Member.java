633a1378418abc8f180acb50c4158cac39f64908
Changho0514 <chh9450@naver.com>
2023-04-29 12:11:33 +0900

    Review Inflearn class : Feat : Domain

---
+++ servlet/src/main/java/mvcReview/servlet/domain/Member/Member.java
@@ -0,0 +1,23 @@
+package mvcReview.servlet.domain.Member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public Member() {
+    }
+
+
+}
---
+++ servlet/src/main/java/mvcReview/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,22 @@
+package mvcReview.servlet.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mvcReview.servlet.domain.Member.MemberRepository;
+
+import java.io.IOException;
+
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+
+    }
+}
